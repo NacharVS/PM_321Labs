914981702e5a9f3952e96606e386ea5444474e3d
r3dinc <r3dinc@yandex.ru>
2022-09-03 13:28:43 +0300

    add new class

---
+++ Labs321/Archer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs321
+{
+    internal class Archer : Unit
+    {
+        public Archer(int health, string name) : base(health, name)
+        {
+        }
+    }
+}
