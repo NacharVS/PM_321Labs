f96bf8cb968b5b0cc842235362837b2edede2513
r3dinc <r3dinc@yandex.ru>
2022-09-05 14:15:15 +0300

    events example

---
+++ Labs320/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Labs320;
+
+TestClass unit = new TestClass(100);
+
+unit.HealthChangedEvent += Rage;
+unit.HealthChangedEvent += Adrenaline;
+
+
+unit.TakeDamage(20);
+unit.TakeDamage(10);
+unit.TakeDamage(15);
+unit.TakeDamage(30);
+
+
+
+static void Rage()
+{
+    Console.WriteLine("Rage is activated!");
+}
+
+static void Adrenaline()
+{
+    Console.WriteLine("Opa opa adrenaline!111");
+}
+
---
+++ Labs320/TestClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs320
+{
+    internal class TestClass
+    {
+        public delegate void HealthChangedDelegate();   
+        
+
+        private int _health;
+
+        public TestClass(int health)
+        {
+            _health = health;
+            MaxHealth = health;
+        }
+
+        public int MaxHealth { get; set; }
+
+        public int Health
+        {
+            get { return _health; }
+            set 
+            {
+                _health = value; 
+
+                if(_health < MaxHealth / 2)
+                {
+                    HealthChangedEvent?.Invoke();
+                }
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"INflicted {damage} - current health{Health}");
+        }
+
+
+        public event HealthChangedDelegate HealthChangedEvent;
+    }
+}
