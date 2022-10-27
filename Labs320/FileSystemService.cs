e74f07368379352d9a8d49bda420f1364611af07
r3dinc <r3dinc@yandex.ru>
2022-10-27 08:54:01 +0300

    gridFS

---
+++ Labs320/FileSystemService.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace BlazorApp7.Data
+{
+    public class FileSystemService
+    {
+        public void UploadImageToDb()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+
+            using (FileStream fs = new FileStream("C:/Users/Vadim.Nacharov/Desktop/images321/ball.jpg", FileMode.Open))
+            {
+                gridFS.UploadFromStream("sss.jpg", fs);
+            }
+        }
+
+        public void DownloadToLocal()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Images/")}DeserializedBall.jpg", FileMode.CreateNew))
+            {
+                gridFS.DownloadToStreamByName("sss.jpg", fs);
+            }
+        }
+    }
+}
