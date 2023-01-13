6a9eebbe958b757e93dff77d04cf46a0c29dd88b
Арсений Анохин <artuhi.arsenii@gmail.com>
2023-01-13 19:36:38 +0300

    task

--- warcraft/Program.cs
+++ warcraft/Program.cs
@@ -3,19 +3,31 @@
 Footman footman2 = new Footman();
 Footman footman = new Footman();
 Mage mage = new Mage();
+Mage mage2 = new Mage();
+//Footman footman = new Footman();
+//Pessant pessant = new Pessant();
+//Pessant pessant2 = new Pessant();
+//Pessant pessant3 = new Pessant();
+//Archer archer = new Archer();
+//BlackSmith bs = new BlackSmith();
+//footman.GetInfo();
+//bs.UpgradeDamage();
+//bs.UpgradeArmor();
+//footman.GetInfo();
+//new Footman().GetInfo();
 
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-footman.Attack(footman2);
-Console.WriteLine("---------------------");
-mage.TargetHeal(footman2);
-mage.TargetHeal(footman2);
+Task.Run(() => BattleMage(mage, mage2));
+Task.Run(() => BattleMage(mage2, mage)).Wait();
+
+
+static void BattleMage(Mage mage, Mage mage2)
+{
+    while (mage.isLive && mage2.isLive)
+    {
+        mage.Attack(mage2);
+    }
+}
+
 
 
 
